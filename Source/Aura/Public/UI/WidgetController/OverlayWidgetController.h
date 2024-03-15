719cd46858ce8c066091e4ed1ad850f2146dcc5e
game-dev-creator <forev3rgamedev@gmail.com>
2024-03-15 14:17:38 +0200

    Callbacks for Mana Changes

--- Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -3,9 +3,10 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UI/WidgetController/AuraWidgetController.h"
 #include <GameplayEffectTypes.h>
+#include "UI/WidgetController/AuraWidgetController.h"
 #include "OverlayWidgetController.generated.h"
+
 
 
 
@@ -13,6 +14,11 @@
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealtChangedSignature, float, NewHealth);
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealtChangedSignature, float, NewMaxHealth);
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnManaChangedSignature, float, NewMana);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxManaChangedSignature, float, NewMaxMana);
 
 /**
  * 
@@ -38,9 +44,22 @@ public:
 	FOnMaxHealtChangedSignature OnMaxHealthChanged;
 
 
+	UPROPERTY(BlueprintAssignable, Category = "GAS|Attributes")
+	FOnManaChangedSignature OnManaChanged;
+
+
+	UPROPERTY(BlueprintAssignable, Category = "GAS|Attributes")
+	FOnMaxManaChangedSignature OnMaxManaChanged;
+
+
 protected:
 
 	void HealthChanged(const FOnAttributeChangeData& Data) const; // Add Definition in OverlayWidgetController.cpp
 
 	void MaxHealthChanged(const FOnAttributeChangeData& Data) const; // Add Definition in OverlayWidgetController.cpp
+
+
+	void ManaChanged(const FOnAttributeChangeData& Data) const; // Add Definition in OverlayWidgetController.cpp
+
+	void MaxManaChanged(const FOnAttributeChangeData& Data) const; // Add Definition in OverlayWidgetController.cpp
 };
